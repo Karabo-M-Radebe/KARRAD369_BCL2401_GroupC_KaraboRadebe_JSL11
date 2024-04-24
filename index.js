0e13cd0bdbbb2762d31b79e0f7417bef0237d7c5
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-04-24 12:07:57 +0200

    added all the necessary elements that I collected form the DOM inside the elements object

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ function initializeData() {
 
 // TASK: Get elements from the DOM
 const elements = {
+  headerBoardName: document.getElementById('header-board-name'),
+  columnDivs: document.querySelector('.column-div'),
+  editTaskModal: document.querySelector('.edit-task-modal-window'),
+  filterDiv: document.getElementById('filterDiv'),
+  hideSideBarBtn: document.getElementById('hide-side-bar-btn'),
+  showSideBarBtn: document.getElementById('show-side-bar-btn'),
+  themeSwitch: document.getElementById('switch'),
+  createNewTaskBtn: document.getElementById('add-new-task-btn'),
+  modalWindow: document.getElementById('new-task-modal-window'),
 
 }
 
