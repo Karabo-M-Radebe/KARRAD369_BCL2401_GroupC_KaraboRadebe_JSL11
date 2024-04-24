f7a9b06368bce202541c7770fc8765636f3ed552
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-04-25 00:24:28 +0200

    made the heading (svg) toggle between the corresponding themes so that it is visible

--- index.js
+++ index.js
@@ -240,10 +240,14 @@ function toggleTheme() {
   if (localStorage.getItem('light-theme') == 'enable'){
     document.body.classList.toggle('light-theme', false);
     localStorage.setItem('light-theme', 'disable');
+    let img = document.getElementById('logo');
+    img.src = './assets/logo-dark.svg';
   } 
   else{
     document.body.classList.toggle('light-theme', true);
     localStorage.setItem('light-theme', 'enable');
+    let img = document.getElementById('logo');
+    img.src = './assets/logo-light.svg';
   }
  
 }
